e754dd17564bf645a691e98bb45dbafb54d3243d
Bakagi <35609996+Bakagi@users.noreply.github.com>
2025-03-23 04:12:32 +0100

    Update Home.blade.php

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <x-Home.hero> </x-home.hero>
+    <x-home.hero> </x-home.hero>
     <x-home.membership></x-home.membership>
     <x-home.about></x-home.about>
     <x-home.contact></x-home.contact>
